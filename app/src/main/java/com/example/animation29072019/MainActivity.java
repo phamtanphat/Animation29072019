29281415b4289e5340901026076206566c6dee3e
phatdroid94 <phatdroid94@gmail.com>
2019-10-30 19:21:34 +0700

    finish rotate bell

--- app/src/main/java/com/example/animation29072019/MainActivity.java
+++ app/src/main/java/com/example/animation29072019/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imgAlpha,imgScale;
+    ImageView imgAlpha,imgScale,imgRotate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
         imgAlpha = findViewById(R.id.imageviewAlpha);
         imgScale = findViewById(R.id.imageviewScale);
+        imgRotate = findViewById(R.id.imageviewRotate);
 
         // 1 - alpha cho chuyen man hinh : intent
 //        final Animation animation = AnimationUtils.loadAnimation(this,R.anim.anim_image_alpha);
@@ -33,9 +34,20 @@ public class MainActivity extends AppCompatActivity {
 
         // x < 50 => chay sang tay phai va nguoc lai
         // y < 50 => chay xuong duoi va nguoc lai
-        final Animation animationScale = AnimationUtils.loadAnimation(this,R.anim.anim_image_scale);
+//        final Animation animationScale = AnimationUtils.loadAnimation(this,R.anim.anim_image_scale);
+//
+//        imgScale.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                v.startAnimation(animationScale);
+//            }
+//        });
+        // 3 : lam thu animation cho clock
+        // y < 50 : toa do nam tren va nguoc lai
+        // x < 50 : toa do nam ben tay trai va nguoc lai
+        final Animation animationScale = AnimationUtils.loadAnimation(this,R.anim.anim_image_rotate);
 
-        imgScale.setOnClickListener(new View.OnClickListener() {
+        imgRotate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 v.startAnimation(animationScale);
