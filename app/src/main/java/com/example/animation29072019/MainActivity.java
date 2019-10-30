f3e54fc6ac10021dba4a9e72cb791acd239080d7
phatdroid94 <phatdroid94@gmail.com>
2019-10-30 18:53:56 +0700

    scale

--- app/src/main/java/com/example/animation29072019/MainActivity.java
+++ app/src/main/java/com/example/animation29072019/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.animation29072019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.transition.Transition;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -10,21 +11,34 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imgAlpha;
+    ImageView imgAlpha,imgScale;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         imgAlpha = findViewById(R.id.imageviewAlpha);
+        imgScale = findViewById(R.id.imageviewScale);
 
-        final Animation animation = AnimationUtils.loadAnimation(this,R.anim.anim_image_alpha);
+        // 1 - alpha cho chuyen man hinh : intent
+//        final Animation animation = AnimationUtils.loadAnimation(this,R.anim.anim_image_alpha);
+//
+//        imgAlpha.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                v.startAnimation(animation);
+//
+//            }
+//        });
 
-        imgAlpha.setOnClickListener(new View.OnClickListener() {
+        // x < 50 => chay sang tay phai va nguoc lai
+        // y < 50 => chay xuong duoi va nguoc lai
+        final Animation animationScale = AnimationUtils.loadAnimation(this,R.anim.anim_image_scale);
+
+        imgScale.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                v.startAnimation(animation);
-
+                v.startAnimation(animationScale);
             }
         });
     }
