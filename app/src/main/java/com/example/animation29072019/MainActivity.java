c4ca2b543a57d6c45c156151e5b21b2f062c0622
phatdroid94 <phatdroid94@gmail.com>
2019-10-30 18:13:50 +0700

    alpha animation

--- app/src/main/java/com/example/animation29072019/MainActivity.java
+++ app/src/main/java/com/example/animation29072019/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.animation29072019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imgAlpha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgAlpha = findViewById(R.id.imageviewAlpha);
+
+        final Animation animation = AnimationUtils.loadAnimation(this,R.anim.anim_image_alpha);
+
+        imgAlpha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(animation);
+
+            }
+        });
     }
 }
